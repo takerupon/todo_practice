56a6484c8ce389225127eef1c8f7dc4371374f51
takerupon <takerup0927@icloud.com>
2023-11-11 00:04:09 +0900

    add todo_page
    ToDoページの作成

--- src/app/todo/page.tsx
+++ src/app/todo/page.tsx
@@ -1,10 +1,31 @@
 "use client"
-import { TodoInput } from '../components/add_todo'
+import { useState } from 'react'
+import  TaskInputForm from '../components/add_todo'
+import  TaskList  from '../components/task_list'
+import { Task } from '../types/taskTypes';
 
-export default function Page() {
+const TodoPage = () => {
+  const [tasks, setTasks] = useState<Task[]>([]); // タスクの状態を管理
+
+  const addTask = (taskTitle: string) => {
+    const newTask = { id: Date.now().toString(), title: taskTitle, completed: false };
+    setTasks([...tasks, newTask]);
+  };
+
+  const updateTask = (taskId: string) => {
+    // タスクの更新ロジック
+  };
+
+  const deleteTask = (taskId: string) => {
+    setTasks(tasks.filter(task => task.id !== taskId));
+  };
+
   return (
-    <>
-      <TodoInput />
-    </>
-  )
-}
+    <div>
+      <TaskInputForm onAddTask={addTask} />
+      <TaskList tasks={tasks} onUpdateTask={updateTask} onDeleteTask={deleteTask} />
+    </div>
+  );
+};
+
+export default TodoPage;
